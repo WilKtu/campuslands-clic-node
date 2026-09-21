51e73a3c45361cc4083b9be84c910a600f5e6afa
WilKtu <fernandocolaj7@gmail.com>
2026-09-21 15:57:56 -0600

    feat(resolucion): ejercicio esm

---
+++ data/campers.js
@@ -0,0 +1,46 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const RUTA_DATA = path.join(__dirname, '..', 'data', 'can...');
+
+export async function LeerCampus() {
+    try {
+        const contenido = await fs.readFile(RUTA_DATA, 'utf-8');
+        return JSON.parse(contenido);
+    } catch (error) {
+        if (error.code === 'ENOENT') { return []; }
+        throw error;
+    }
+}
+export async function guardarCampers(lista) {
+    await fs.writeFile(RUTA_DATA, JSON.stringify(lista, null, 2),'utf-8')
+}
+
+export async function agregar(nombre, stack) {
+    if (!nombre || !stack){
+        throw new error('Debes ingresar nombre y stack')
+    }
+    const campers = await LeerCampers();
+    const nuevoCamper = {
+        id: campers.length > 0 ? campers[campers.length -1].id +1:1,
+        nombre : nombre.trin(),stack: stack.trin(),
+        creadoEn: new Date().toLocaleDateString()
+    };
+
+    campers.push(nuevoCamper);
+    await guardarCampers(campers);
+    return nuevoCamper;
+
+}
+export async function listarCamper() {
+    return await LeerCampers();
+}
+export async function buscarCamperPorNombre(termino) {
+    const campers = await LeerCampers();
+    return campers.filter(c => c.nombre.toLowerCase().includes(ternimo.toLowerCase()));
+}
